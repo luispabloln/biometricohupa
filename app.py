f95f48e9fa556ec517d5e8679f0cd9957c4ac083
luispabloln <65974204+luispabloln@users.noreply.github.com>
2026-01-06 12:27:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ st.set_page_config(
 # --- SEGURIDAD Y USUARIOS ---
 CREDENCIALES = {
     "luisln": "Luisln2227",
+    "joseh": "JoseH!123",
     "admin": "admin123",
     "gerencia": "gerencia2025",
     "rrhh": "rrhh123"
@@ -298,4 +299,5 @@ if not df_res.empty:
         st.dataframe(df_det.sort_values(['Fecha', 'Nombre'], ascending=[False, True]), use_container_width=True, hide_index=True)
 else:
     st.warning("No hay datos para mostrar.")
+
 
