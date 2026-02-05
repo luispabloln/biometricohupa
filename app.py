09bb879bc7f066dcd32ec517875ba686adc67d56
luispabloln <65974204+luispabloln@users.noreply.github.com>
2026-02-05 11:03:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.set_page_config(
 # --- SEGURIDAD Y USUARIOS ---
 CREDENCIALES = {
     "admin": "admin123",
-    "gerencia": "gerencia2026",
+    "joseh": "Jose123",
     "rrhh": "rrhh123"
 }
 
@@ -347,4 +347,5 @@ if not df_res.empty:
         st.dataframe(df_det.sort_values(['Fecha', 'Nombre'], ascending=[False, True]), use_container_width=True, hide_index=True)
 else:
     st.warning("No se encontraron coincidencias. Verifica que los IDs en 'usuarios.csv' coincidan exactamente con 'registros.csv'.")
+
 
