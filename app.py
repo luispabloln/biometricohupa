14a31bdbe98e3adf52be0dd0db2352afa20da2a2
luispabloln <65974204+luispabloln@users.noreply.github.com>
2026-03-03 12:49:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ CREDENCIALES = {
     "admin": "admin123",
     "gerencia": "gerencia2025",
     "rrhh": "rrhh123"
+    "joseh": "JoseH!123"
 }
 
 def check_password():
@@ -343,3 +344,4 @@ if not df_res.empty:
         st.dataframe(df_det.sort_values(['Fecha', 'Nombre'], ascending=[False, True]), use_container_width=True, hide_index=True)
 else:
     st.warning("No se encontraron coincidencias. Verifica que los IDs en 'usuarios.csv' coincidan exactamente con 'registros.csv'.")
+
